9aab4b24e5ba4d38724e4185708464c12b0249ce
SilasMahler <simah002@stud.fh-dortmund.de>
2019-08-06 15:08:56 +0200

    add basic sso backend

---
+++ backend/src/main/java/de/dohack/githubbot/backend/SecurityConfig.java
@@ -0,0 +1,38 @@
+package de.dohack.githubbot.backend;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().disable();
+    }
+
+    @Bean
+    public PrincipalExtractor githubPrincipalExtractor() {
+        return new GithubPrincipalExtractor();
+    }
+
+    @Bean
+    public AuthoritiesExtractor githubAuthoritiesExtractor() {
+        return new GithubAuthoritiesExtractor();
+    }
+}
