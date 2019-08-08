57a6f93ea6072d310ca40e5ee225ee82f6b10b22
SilasMahler <simah002@stud.fh-dortmund.de>
2019-08-08 17:36:21 +0200

    adjust configs

--- backend/src/main/java/de/dohack/githubbot/backend/SecurityConfig.java
+++ backend/src/main/java/de/dohack/githubbot/backend/SecurityConfig.java
@@ -5,14 +5,23 @@ import org.springframework.boot.autoconfigure.security.oauth2.resource.Authoriti
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
+/*@EnableOAuth2Sso*/
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+
+    /*@Override
     protected void configure(HttpSecurity http)
             throws Exception {
 
@@ -24,6 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .formLogin().disable();
+    }*/
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
