f188c32b74f7172c825b2a122e1b70872c325769
SilasMahler <simah002@stud.fh-dortmund.de>
2019-08-12 10:05:50 +0200

    init version

--- backend/src/main/java/de/dohack/githubbot/backend/SecurityConfig.java
+++ backend/src/main/java/de/dohack/githubbot/backend/SecurityConfig.java
@@ -1,46 +1,61 @@
 package de.dohack.githubbot.backend;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 
 @Configuration
-/*@EnableOAuth2Sso*/
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        /*http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login();
-    }
-
-    /*@Override
-    protected void configure(HttpSecurity http)
-            throws Exception {
-
-        http.antMatcher("/**")
+                .oauth2Login();*/
+        http
+                .antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/login**")
+                .antMatchers("/", "/login**","/callback/", "/webjars/**", "/error**","/oauth_login", "/loginFailure")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin().disable();
-    }*/
+                .oauth2Login()
+                .loginPage("/oauth_login")
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorize-client")
+                .authorizationRequestRepository(authorizationRequestRepository())
+                .and()
+                .tokenEndpoint()
+                .accessTokenResponseClient(accessTokenResponseClient())
+                .and()
+                .defaultSuccessUrl("/loginSuccess")
+                .failureUrl("/loginFailure");
 
-    @Override
+        }
+
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
     }
 
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient = new DefaultAuthorizationCodeTokenResponseClient();
+        return accessTokenResponseClient;
+    }
+}
+    /*
     @Bean
     public PrincipalExtractor githubPrincipalExtractor() {
         return new GithubPrincipalExtractor();
@@ -49,5 +64,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthoritiesExtractor githubAuthoritiesExtractor() {
         return new GithubAuthoritiesExtractor();
-    }
-}
+    }*/
